680d59254c7503351ba30e7c848495504e65110f
Mareike <mareike.brandt@hotmail.de>
2022-10-20 01:20:45 +0900

    feat: About section complete

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { images } from "../../constants";
-
+import { AppWrap } from "../../wrapper";
 import "./Header.scss";
 
 const Header = () => {
@@ -69,4 +69,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default AppWrap(Header, "home");
